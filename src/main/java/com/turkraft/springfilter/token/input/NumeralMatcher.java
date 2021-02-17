c8289c6904a0252b8d25c8c9823397beb517c10e
torshid <torshid@gmail.com>
2021-02-17 14:20:10 +0300

    Disabling number format grouping

--- src/main/java/com/turkraft/springfilter/token/input/NumeralMatcher.java
+++ src/main/java/com/turkraft/springfilter/token/input/NumeralMatcher.java
@@ -2,6 +2,7 @@ package com.turkraft.springfilter.token.input;
 
 import java.text.NumberFormat;
 import java.text.ParsePosition;
+import java.util.Locale;
 
 import com.turkraft.springfilter.Extensions;
 import com.turkraft.springfilter.token.Matcher;
@@ -11,12 +12,19 @@ import lombok.experimental.ExtensionMethod;
 @ExtensionMethod(Extensions.class)
 public class NumeralMatcher extends Matcher<Numeral> {
 
+  private static final NumberFormat NUMBER_FORMAT;
+
+  static {
+    NUMBER_FORMAT = NumberFormat.getInstance(Locale.US);
+    NUMBER_FORMAT.setGroupingUsed(false);
+  }
+
   @Override
   public Numeral match(StringBuilder input) {
 
     ParsePosition position = new ParsePosition(0);
 
-    Number number = NumberFormat.getInstance().parse(input.toString(), position);
+    Number number = NUMBER_FORMAT.parse(input.toString(), position);
 
     if (number != null) {
       input.take(position.getIndex());
