62c7f0d38c1fb6ddf23d1a0c23d08fd0076bebfe
torshid <torshid@gmail.com>
2021-05-03 16:04:36 +0300

    Fix test app

--- src/test/java/com/turkraft/springfilter/jpa/Application.java
+++ src/test/java/com/turkraft/springfilter/jpa/Application.java
@@ -12,8 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,7 +24,6 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@SpringBootApplication(exclude = EmbeddedMongoAutoConfiguration.class)
 @RestController
 @Import({org.springdoc.core.SpringDocConfigProperties.class,
     org.springdoc.webmvc.core.MultipleOpenApiSupportConfiguration.class,
