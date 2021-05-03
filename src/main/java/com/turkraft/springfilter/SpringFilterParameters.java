71b58b2da05666ca757383c5ba58c8f88307a89e
torshid <torshid@gmail.com>
2021-05-03 15:00:56 +0300

    Support for java.time.*

--- src/main/java/com/turkraft/springfilter/SpringFilterParameters.java
+++ src/main/java/com/turkraft/springfilter/SpringFilterParameters.java
@@ -2,6 +2,7 @@ package com.turkraft.springfilter;
 
 import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class SpringFilterParameters {
@@ -9,12 +10,18 @@ public class SpringFilterParameters {
   private SpringFilterParameters() {}
 
   public static SimpleDateFormat DATE_FORMATTER;
+  public static DateTimeFormatter LOCALDATE_FORMATTER;
+  public static DateTimeFormatter LOCALDATETIME_FORMATTER;
+  public static DateTimeFormatter OFFSETDATETIME_FORMATTER;
 
   public static NumberFormat NUMBER_FORMAT;
 
   static {
 
     DATE_FORMATTER = new SimpleDateFormat("dd-MM-yyyy");
+    LOCALDATE_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    LOCALDATETIME_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy'T'HH:mm:ss");
+    OFFSETDATETIME_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy'T'HH:mm:ss.SSSXXX");
 
     NUMBER_FORMAT = NumberFormat.getInstance(Locale.US);
     NUMBER_FORMAT.setGroupingUsed(false); // in order to not count commas as part of number
--- src/main/java/com/turkraft/springfilter/boot/SpringFilterAutoConfig.java
+++ src/main/java/com/turkraft/springfilter/boot/SpringFilterAutoConfig.java
@@ -1,5 +1,6 @@
 package com.turkraft.springfilter.boot;
 
+import java.time.format.DateTimeFormatter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import com.turkraft.springfilter.SpringFilterParameters;
@@ -8,10 +9,27 @@ import com.turkraft.springfilter.SpringFilterParameters;
 public class SpringFilterAutoConfig {
 
   public SpringFilterAutoConfig(
-      @Value("${turkraft.springfilter.dateformatter.pattern:#{null}}") String pattern) {
+      @Value("${turkraft.springfilter.dateformatter.pattern:#{null}}") String datePattern,
+      @Value("${turkraft.springfilter.localdateformatter.pattern:#{null}}") String localDatePattern,
+      @Value("${turkraft.springfilter.localdatetimeformatter.pattern:#{null}}") String localDateTimePattern,
+      @Value("${turkraft.springfilter.offsetdatetimeformatter.pattern:#{null}}") String offsetDateTimePattern) {
 
-    if (pattern != null) {
-      SpringFilterParameters.DATE_FORMATTER.applyPattern(pattern);
+    if (datePattern != null) {
+      SpringFilterParameters.DATE_FORMATTER.applyPattern(datePattern);
+    }
+
+    if (localDatePattern != null) {
+      SpringFilterParameters.LOCALDATE_FORMATTER = DateTimeFormatter.ofPattern(localDatePattern);
+    }
+
+    if (localDateTimePattern != null) {
+      SpringFilterParameters.LOCALDATETIME_FORMATTER =
+          DateTimeFormatter.ofPattern(localDateTimePattern);
+    }
+
+    if (offsetDateTimePattern != null) {
+      SpringFilterParameters.OFFSETDATETIME_FORMATTER =
+          DateTimeFormatter.ofPattern(offsetDateTimePattern);
     }
 
   }
