d254af9578eb958ec99156a54a6710129ab137e0
torshid <torshid@gmail.com>
2021-09-26 23:26:39 +0300

    No more `up`

--- src/main/java/com/turkraft/springfilter/FilterBuilder.java
+++ src/main/java/com/turkraft/springfilter/FilterBuilder.java
@@ -27,8 +27,6 @@ import com.turkraft.springfilter.compiler.token.input.Text;
 
 public class FilterBuilder {
 
-  public static final String UP = "up";
-
   /* FILTER */
 
   public static Filter filter(IExpression body) {
