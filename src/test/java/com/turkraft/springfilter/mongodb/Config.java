15902ff2d353aa2b17afe204218ef264b595900b
torshid <torshid@gmail.com>
2021-10-06 11:06:51 +0300

    Mongo app config update

--- src/test/java/com/turkraft/springfilter/mongodb/Config.java
+++ src/test/java/com/turkraft/springfilter/mongodb/Config.java
@@ -8,8 +8,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import com.mongodb.client.MongoClients;
 import de.flapdoodle.embed.mongo.MongodExecutable;
 import de.flapdoodle.embed.mongo.MongodStarter;
-import de.flapdoodle.embed.mongo.config.IMongodConfig;
-import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
+import de.flapdoodle.embed.mongo.config.MongodConfig;
 import de.flapdoodle.embed.mongo.config.Net;
 import de.flapdoodle.embed.mongo.distribution.Version;
 import de.flapdoodle.embed.process.runtime.Network;
@@ -28,7 +27,7 @@ public class Config implements InitializingBean, DisposableBean {
     String ip = "localhost";
     int port = 27017;
 
-    IMongodConfig mongodConfig = new MongodConfigBuilder().version(Version.Main.PRODUCTION)
+    MongodConfig mongodConfig = MongodConfig.builder().version(Version.Main.PRODUCTION)
         .net(new Net(ip, port, Network.localhostIsIPv6())).build();
 
     MongodStarter starter = MongodStarter.getDefaultInstance();
