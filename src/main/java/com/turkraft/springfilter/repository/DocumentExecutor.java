03319eec2b552a784305490d4468ab52a45302be
Linus Nibell <linus.nibell@consultant.volvo.com>
2022-05-19 10:48:49 +0200

    Changed DocumentExecutor to match MongoRepository

--- src/main/java/com/turkraft/springfilter/repository/DocumentExecutor.java
+++ src/main/java/com/turkraft/springfilter/repository/DocumentExecutor.java
@@ -4,48 +4,21 @@ import java.util.List;
 import java.util.Optional;
 import org.bson.Document;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.repository.query.MongoEntityInformation;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.lang.Nullable;
-import com.turkraft.springfilter.parser.generator.bson.BsonGeneratorUtils;
 
 @NoRepositoryBean
-public interface DocumentExecutor<T, I> {
+public interface DocumentExecutor<T, ID> {
 
-  default Optional<T> findOne(@Nullable Document doc) {
-    return Optional.ofNullable(getMongoOperations()
-        .findOne(BsonGeneratorUtils.getQueryFromDocument(doc), getMetadata().getJavaType()));
-  }
+  Optional<T> findOne(@Nullable Document doc);
 
-  default List<T> findAll(@Nullable Document doc) {
-    return getMongoOperations().find(BsonGeneratorUtils.getQueryFromDocument(doc),
-        getMetadata().getJavaType());
-  }
+  List<T> findAll(@Nullable Document doc);
 
-  default Page<T> findAll(@Nullable Document doc, Pageable pageable) {
-    Query query = BsonGeneratorUtils.getQueryFromDocument(doc).with(pageable);
-    long count = getMongoOperations().count(query, getMetadata().getJavaType());
-    List<T> content = getMongoOperations().find(query, getMetadata().getJavaType());
-    return new PageImpl<>(content, pageable, count);
-  }
+  Page<T> findAll(@Nullable Document doc, Pageable pageable);
 
-  default List<T> findAll(@Nullable Document doc, Sort sort) {
-    return getMongoOperations().find(BsonGeneratorUtils.getQueryFromDocument(doc).with(sort),
-        getMetadata().getJavaType());
-  }
+  List<T> findAll(@Nullable Document doc, Sort sort);
 
-  default long count(@Nullable Document doc) {
-    return getMongoOperations().count(BsonGeneratorUtils.getQueryFromDocument(doc),
-        getMetadata().getJavaType());
-  }
-
-  MongoEntityInformation<T, I> getMetadata();
-
-  MongoOperations getMongoOperations();
-
+  long count(@Nullable Document doc);
 }
