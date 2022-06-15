547428c8618159ab795a7b616613320b7875db74
Linus Nibell <linus.nibell@consultant.volvo.com>
2022-06-15 14:44:02 +0200

    restored test

--- src/test/java/com/turkraft/springfilter/mongodb/CarRepository.java
+++ src/test/java/com/turkraft/springfilter/mongodb/CarRepository.java
@@ -1,8 +1,30 @@
 package com.turkraft.springfilter.mongodb;
 
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.repository.query.MongoEntityInformation;
+import org.springframework.data.mongodb.repository.support.MongoRepositoryFactory;
 import org.springframework.stereotype.Repository;
 import com.turkraft.springfilter.repository.DocumentExecutor;
 
 @Repository
-public interface CarRepository extends DocumentExecutor<Car, Long> {
+public class CarRepository implements DocumentExecutor<Car, Long> {
+
+  private final MongoEntityInformation<Car, Long> metadata;
+  private final MongoOperations mongoOperations;
+
+  public CarRepository(MongoOperations mongoOperations) {
+    this.mongoOperations = mongoOperations;
+    this.metadata = new MongoRepositoryFactory(mongoOperations).getEntityInformation(Car.class);
+  }
+
+  @Override
+  public MongoEntityInformation<Car, Long> getMetadata() {
+    return metadata;
+  }
+
+  @Override
+  public MongoOperations getMongoOperations() {
+    return mongoOperations;
+  }
+
 }
