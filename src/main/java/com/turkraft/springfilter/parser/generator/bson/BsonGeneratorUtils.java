94073a46411bcbe16cb31e4c1f2b0d01e43e2988
Linus Nibell <linus.nibell@consultant.volvo.com>
2022-06-27 09:22:32 +0200

    reverted BsonGeneratorUtils

--- src/main/java/com/turkraft/springfilter/parser/generator/bson/BsonGeneratorUtils.java
+++ src/main/java/com/turkraft/springfilter/parser/generator/bson/BsonGeneratorUtils.java
@@ -5,22 +5,32 @@ import org.bson.BsonDocumentReader;
 import org.bson.Document;
 import org.bson.codecs.DecoderContext;
 import org.bson.codecs.DocumentCodec;
+import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.conversions.Bson;
 import org.springframework.data.mongodb.core.query.BasicQuery;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.lang.Nullable;
 
 public class BsonGeneratorUtils {
 
   private BsonGeneratorUtils() {}
 
   public static Document getDocumentFromBson(Bson bson) {
+    return getDocumentFromBson(bson, null);
+  }
+
+  public static Document getDocumentFromBson(Bson bson, @Nullable CodecRegistry codecRegistry) {
 
     if (bson == null) {
       return null;
     }
 
+    if (codecRegistry == null) {
+      codecRegistry = BsonGeneratorParameters.getCodecRegistry();
+    }
+
     BsonDocument bsonDocument =
-        bson.toBsonDocument(BsonDocument.class, BsonGeneratorParameters.getCodecRegistry());
+        bson.toBsonDocument(BsonDocument.class, codecRegistry);
     DocumentCodec codec = new DocumentCodec();
     DecoderContext decoderContext = DecoderContext.builder().build();
     return codec.decode(new BsonDocumentReader(bsonDocument), decoderContext);
