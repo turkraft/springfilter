27de833850d9185c3ff196fbd47b308e771fa99a
torshid <torshid@gmail.com>
2023-11-20 19:40:50 +0300

    Support for `like`, `ilike`, `>=`, `<=`, `<>`

--- core/src/main/java/com/turkraft/springfilter/language/EqualOperator.java
+++ core/src/main/java/com/turkraft/springfilter/language/EqualOperator.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 public class EqualOperator extends FilterInfixOperator {
 
   public EqualOperator() {
-    super(":", 100);
+    super(new String[]{":", "="}, 100);
   }
 
 }
