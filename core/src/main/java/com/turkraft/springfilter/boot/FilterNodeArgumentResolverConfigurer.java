7cf867b1272daf5924fd5442a1b72b504a1a4e5c
torshid <torshid@gmail.com>
2024-11-20 21:30:43 +0300

    Conditional WebMvc configuration

--- core/src/main/java/com/turkraft/springfilter/boot/FilterNodeArgumentResolverConfigurer.java
+++ core/src/main/java/com/turkraft/springfilter/boot/FilterNodeArgumentResolverConfigurer.java
@@ -1,11 +1,13 @@
 package com.turkraft.springfilter.boot;
 
 import java.util.List;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Component
+@Conditional(WebMvcConfigurerCondition.class)
 public class FilterNodeArgumentResolverConfigurer implements WebMvcConfigurer {
 
   protected final FilterNodeArgumentResolverHelper filterNodeArgumentResolverHelper;
@@ -14,7 +16,6 @@ public class FilterNodeArgumentResolverConfigurer implements WebMvcConfigurer {
       FilterNodeArgumentResolverHelper filterNodeArgumentResolverHelper) {
     this.filterNodeArgumentResolverHelper = filterNodeArgumentResolverHelper;
   }
-
 
   @Override
   public void addArgumentResolvers(
