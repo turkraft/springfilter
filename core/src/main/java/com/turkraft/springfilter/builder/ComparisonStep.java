ee9e0b707e44e3534bd45e1fb4d6f0b76c6d9970
torshid <torshid@gmail.com>
2025-11-05 12:16:24 +0300

    Add missing step

--- core/src/main/java/com/turkraft/springfilter/builder/ComparisonStep.java
+++ core/src/main/java/com/turkraft/springfilter/builder/ComparisonStep.java
@@ -1,6 +1,6 @@
 package com.turkraft.springfilter.builder;
 
 public interface ComparisonStep extends LikeStep, InsensitiveLikeStep, EqualStep, NotEqualStep, GreaterThanStep, GreaterThanOrEqualStep,
-    LessThanStep, LessThanOrEqualStep, IsNullStep, IsNotNullStep, IsEmptyStep, IsNotEmptyStep, InStep {
+    LessThanStep, LessThanOrEqualStep, IsNullStep, IsNotNullStep, IsEmptyStep, IsNotEmptyStep, InStep, NotInStep {
 
 }
