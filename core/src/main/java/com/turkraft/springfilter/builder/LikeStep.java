56c4c3c1bf6192831d89d54a7ad5bc0ea408181b
torshid <torshid@gmail.com>
2026-08-06 17:48:11 +0300

    Practical like methods added

--- core/src/main/java/com/turkraft/springfilter/builder/LikeStep.java
+++ core/src/main/java/com/turkraft/springfilter/builder/LikeStep.java
@@ -4,6 +4,7 @@ import com.turkraft.springfilter.builder.AbstractStep.StepWithResultImpl;
 import com.turkraft.springfilter.definition.FilterOperators;
 import com.turkraft.springfilter.language.LikeOperator;
 import com.turkraft.springfilter.parser.node.FilterNode;
+import com.turkraft.springfilter.parser.node.InputNode;
 
 public interface LikeStep extends StepWithResult {
 
@@ -11,6 +12,18 @@ public interface LikeStep extends StepWithResult {
     return new LikeStepImpl(getOperators(),
         get().infix(getOperators().getInfixOperator(LikeOperator.class),
             to.get()));
+  }
+
+  default LikeStepImpl startsWith(String value) {
+    return like(new StepWithResultImpl(getOperators(), new InputNode(value + "%")));
+  }
+
+  default LikeStepImpl endsWith(String value) {
+    return like(new StepWithResultImpl(getOperators(), new InputNode("%" + value)));
+  }
+
+  default LikeStepImpl contains(String value) {
+    return like(new StepWithResultImpl(getOperators(), new InputNode("%" + value + "%")));
   }
 
   class LikeStepImpl extends StepWithResultImpl implements LogicStep {
